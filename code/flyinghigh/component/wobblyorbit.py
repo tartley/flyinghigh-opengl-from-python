97470421b3c3b66c1f1a966139b51f467bf8efa2
Jonathan Hartley <tartley@tartley.com>
2010-06-06 11:09:24 +0100

    WobblyOrbit now moves slower when camera nears the origin

--- code/flyinghigh/component/wobblyorbit.py
+++ code/flyinghigh/component/wobblyorbit.py
@@ -7,20 +7,32 @@ class WobblyOrbit(object):
 
     def __init__(self):
         self.age = 0.0
+        self.position = None
 
     def __call__(self, dt):
-        self.age += dt
+        # time moves slower when camera is near the origin
+        if (
+            self.position and
+            abs(self.position.x) < 20 and
+            abs(self.position.y) < 20 and
+            abs(self.position.z) < 20
+        ):
+            self.age += dt / 4
+        else:
+            self.age += dt
 
+        # camera position at distance, bearing
         bearing = cos(self.age / 5 - pi/2) * 10
         distance = 80 + sin(self.age / 3) * 75
-        elevation = sin(self.age) * cos(self.age / 3) * pi / 2
-
         x1 = distance * sin(bearing)
         z1 = distance * cos(bearing)
 
+        # then elevate above the x = z = 0 plane
+        elevation = sin(self.age) * cos(self.age / 3) * pi / 2
         x2 = x1 * cos(elevation)
         z2 = z1 * cos(elevation)
         y2 = distance * sin(elevation)
 
-        return Vec3(x2, y2, z2)
+        self.position = Vec3(x2, y2, z2)
+        return self.position
         
