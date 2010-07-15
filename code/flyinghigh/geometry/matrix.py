7e09f730387a93013d667e6b14b13425d978de08
Jonathan Hartley <tartley@tartley.com>
2010-07-15 22:40:44 +0100

    optimise Matrix in case where no orientation is needed

--- code/flyinghigh/geometry/matrix.py
+++ code/flyinghigh/geometry/matrix.py
@@ -1,20 +1,18 @@
 
-from .vec3 import Origin, Vec3
-from .orientation import Orientation
+from .vec3 import Vec3
 
 
 class Matrix(object):
 
     def __init__(self, position, orientation=None):
-        if position is None:
-            position = Origin
-        if orientation is None:
-            orientation = Orientation()
-        
-        self.elements = orientation.matrix
-        self.elements[3] = position.x
-        self.elements[7] = position.y
-        self.elements[11] = position.z
+        self.position = position
+        self.orientation = orientation
+        if orientation:
+            self.elements = orientation.matrix
+        if orientation and position:
+            self.elements[3] = position.x
+            self.elements[7] = position.y
+            self.elements[11] = position.z
 
 
     def transform(self, vert):
@@ -22,10 +20,19 @@ class Matrix(object):
         return the product of the given vertex by self, to give the vertex
         rotated and by our orientation and translated by our position.
         '''
-        e = self.elements
-        return Vec3(
-            vert.x * e[0] + vert.y * e[1] + vert.z * e[2]   + e[3],
-            vert.x * e[4] + vert.y * e[5] + vert.z * e[6]   + e[7],
-            vert.x * e[8] + vert.y * e[9] + vert.z * e[10]  + e[11],
-        )
+        if self.orientation:
+            e = self.elements
+            return Vec3(
+                vert.x * e[0] + vert.y * e[1] + vert.z * e[2]   + e[3],
+                vert.x * e[4] + vert.y * e[5] + vert.z * e[6]   + e[7],
+                vert.x * e[8] + vert.y * e[9] + vert.z * e[10]  + e[11],
+            )
+        else:
+            if self.position:
+                return Vec3(
+                    vert.x + self.position.x,
+                    vert.y + self.position.y,
+                    vert.z + self.position.z)
+            else:
+                return vert
 
